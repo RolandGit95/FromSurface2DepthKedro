59016b6ce737658629ef2944b3222947d009da02
RolandGit95 <rolandstenger95@gmail.com>
2021-09-13 11:19:56 +0200

    updated

--- src/pydiver/pipeline_registry.py
+++ src/pydiver/pipeline_registry.py
@@ -40,26 +40,28 @@ from pydiver.pipelines import model_validation as mv
         
 def register_pipelines() -> Dict[str, Pipeline]:
 
-    data_simulation_pipeline_A = ds.create_pipeline(regime="A")
-    data_processing_pipeline_A = dp.create_pipeline(regime="A")
+    #data_simulation_pipeline_A = ds.create_pipeline(regime="A")
+    #data_processing_pipeline_A = dp.create_pipeline(regime="A")
     model_validation_pipeline_A = mv.create_pipeline(regime="A")
 
     
-    data_simulation_pipeline_B = ds.create_pipeline(regime="B")
-    data_processing_pipeline_B = dp.create_pipeline(regime="B")
-    model_validation_pipeline_B = mv.create_pipeline(regime="B")
+    #data_simulation_pipeline_B = ds.create_pipeline(regime="B")
+    #data_processing_pipeline_B = dp.create_pipeline(regime="B")
+    #model_validation_pipeline_B = mv.create_pipeline(regime="B")
     #data_scient_pipeline = ds.create_pipeline()
 
     
     return {
-        "__default__": data_simulation_pipeline_A + data_processing_pipeline_A + model_validation_pipeline_A,
-        "ds_A": data_simulation_pipeline_A,
-        "dp_A": data_processing_pipeline_A,
+        #"__default__": data_simulation_pipeline_A + data_processing_pipeline_A + model_validation_pipeline_A,
+        "__default__": model_validation_pipeline_A,
+
+        #"ds_A": data_simulation_pipeline_A,
+        #"dp_A": data_processing_pipeline_A,
         "mv_A": model_validation_pipeline_A,
 
-        "ds_B": data_simulation_pipeline_B,
-        "dp_B": data_processing_pipeline_B,
-        "mv_B": model_validation_pipeline_B,
+        #"ds_B": data_simulation_pipeline_B,
+        #"dp_B": data_processing_pipeline_B,
+        #"mv_B": model_validation_pipeline_B,
             }
 
 
