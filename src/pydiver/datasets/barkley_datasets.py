34a1d913ad7ae6113983d781e25ec297170de6d3
RolandGit95 <rolandstenger95@gmail.com>
2021-09-14 12:51:18 +0200

    updated

--- src/pydiver/datasets/barkley_datasets.py
+++ src/pydiver/datasets/barkley_datasets.py
@@ -4,12 +4,15 @@ from torch.utils.data import Dataset
 # %%
 
 class InputDataset(Dataset):
-    def __init__(self, data):
+    def __init__(self, data, transform=lambda data:(data.float()+127.)/255.):
         self.data = data
-        self.transform = lambda data:(data.float()+127)/255.
+        self.transform = transform
 
     def __getitem__(self , index):
-         return self.transform(self.data[index])
+        if self.transform:
+            return self.transform(self.data[index])
+        else:
+            return self.data[index]
 
     def __len__(self):
         return len(self.data)
@@ -21,7 +24,7 @@ class TestDataset(Dataset):
     def __init__(self, ):
         super(TestDataset, self).__init__(root='') 
         
-        self.transform = lambda data:(data.float()+127)/255.
+        self.transform = lambda data:(data.float()+127.)/255.
 
     def __getitem__(self, idx):
         return self.transform(self.X[idx]), self.transform(self.Y[idx])
--- src/pydiver/pipelines/model_validation/utils.py
+++ src/pydiver/pipelines/model_validation/utils.py
@@ -32,28 +32,34 @@ def predict(model, data, depths=[0,1,2], batch_size=8, device="cuda"):
     
     return y_preds
 
-def validate(y_true, y_pred, depths=[0,1,2], loss_function="mae", batch_size=128):
+
+def validate(y_true, y_pred, depths=[0,1,2], loss_function="mae", batch_size=8):
     assert loss_function in loss_functions, f"Loss function {loss_function} not implemented"
     assert y_pred.shape[2] == len(depths), "Number of layers to be validated don't match with the prediction-dimensions"
-    assert len(y_true) == len(y_pred), "Two dataset don't have the same length"
+    #assert len(y_true) == len(y_pred), "Two dataset don't have the same length"
 
     true_dataset = InputDataset(torch.from_numpy(y_true))
     true_dataloader = DataLoader(true_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
-    pred_dataset = InputDataset(torch.from_numpy(y_pred))
+    pred_dataset = InputDataset(torch.from_numpy(y_pred), transform=None)
     pred_dataloader = DataLoader(pred_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
     loss_fnc = loss_functions[loss_function]
 
-    for _y_true, _y_pred in zip(true_dataloader, pred_dataloader):
+    LOSSES = []
+    for _y_true, _y_pred in tqdm(zip(true_dataloader, pred_dataloader), total=len(true_dataloader)):
         losses = []
         for i, depth in enumerate(depths):
             #import IPython ; IPython.embed() ; exit(1)
             #print(_y_pred.shape, loss_fnc)
             loss = loss_fnc(_y_true[:,:,depth].cpu().detach().numpy(), _y_pred[:,:,i].cpu().detach().numpy())
+            #print(loss)
             losses.append(loss)
-
-    return np.array(losses)
+        LOSSES.append(np.array(losses))
+    LOSSES = np.array(LOSSES)
+    means = np.mean(LOSSES, axis=0)
+    #print(means)
+    return means
     
     
     
