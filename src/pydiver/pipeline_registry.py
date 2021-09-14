5a2a8712a1e6b2d1aa8f5a3f7f1bdf7ba882343d
RolandGit95 <rolandstenger95@gmail.com>
2021-09-14 11:10:03 +0200

    updated

--- src/pydiver/pipeline_registry.py
+++ src/pydiver/pipeline_registry.py
@@ -1,37 +1,10 @@
-# Copyright 2021 QuantumBlack Visual Analytics Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
-# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
-# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-# The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
-# (either separately or in combination, "QuantumBlack Trademarks") are
-# trademarks of QuantumBlack. The License does not grant you any right or
-# license to the QuantumBlack Trademarks. You may not use the QuantumBlack
-# Trademarks or any confusingly similar mark as a trademark for your product,
-# or use the QuantumBlack Trademarks in any other manner that might cause
-# confusion in the marketplace, including but not limited to in advertising,
-# on websites, or on software.
-#
-# See the License for the specific language governing permissions and
-# limitations under the License.
-        
-        
 
 """Project pipelines."""
 from typing import Dict
 
+import kedro
 from kedro.pipeline import Pipeline
+from kedro.config import ConfigLoader
 
 from pydiver.pipelines import data_simulation as ds
 from pydiver.pipelines import data_processing as dp
@@ -40,29 +13,52 @@ from pydiver.pipelines import model_validation as mv
         
 def register_pipelines() -> Dict[str, Pipeline]:
 
+    #conf_paths = ['conf/base/']
+    #conf_loader = ConfigLoader(conf_paths)
+
+    registered_pipelines = {}
+    #try:
+    #    multi_model_eval_kwargs = conf_loader.get('multi_model_heval.yml')
+    #    model_validation_pipeline = mv.create_pipeline(multi_model_eval=multi_model_eval_kwargs)
+        
+    #except kedro.config.config.MissingConfigException:
+    #    print('No multi_model_eval')
+
+    #    model_validation_pipeline = mv.create_pipeline(regime="A")
+
+    model_validation_pipeline = mv.create_pipeline(regime="A")
+
+    registered_pipelines['mv_A'] = model_validation_pipeline
+    registered_pipelines['__default__'] = model_validation_pipeline
+
+    
     #data_simulation_pipeline_A = ds.create_pipeline(regime="A")
     #data_processing_pipeline_A = dp.create_pipeline(regime="A")
-    model_validation_pipeline_A = mv.create_pipeline(regime="A")
+    #model_validation_pipeline_A = mv.create_pipeline(regime="A")
 
+    #multi_model_validation_pipeline
     
     #data_simulation_pipeline_B = ds.create_pipeline(regime="B")
     #data_processing_pipeline_B = dp.create_pipeline(regime="B")
     #model_validation_pipeline_B = mv.create_pipeline(regime="B")
     #data_scient_pipeline = ds.create_pipeline()
 
-    
-    return {
+    #import IPython ; IPython.embed() ; exit(1)
+
+    return registered_pipelines
+
+    #return {
         #"__default__": data_simulation_pipeline_A + data_processing_pipeline_A + model_validation_pipeline_A,
-        "__default__": model_validation_pipeline_A,
+    #    "__default__": model_validation_pipeline_A,
 
         #"ds_A": data_simulation_pipeline_A,
         #"dp_A": data_processing_pipeline_A,
-        "mv_A": model_validation_pipeline_A,
+    #    "mv_A": model_validation_pipeline_A,
 
         #"ds_B": data_simulation_pipeline_B,
         #"dp_B": data_processing_pipeline_B,
         #"mv_B": model_validation_pipeline_B,
-            }
+     #       }
 
 
 
--- src/pydiver/pipelines/model_validation/pipeline.py
+++ src/pydiver/pipelines/model_validation/pipeline.py
@@ -1,8 +1,11 @@
-from kedro.pipeline import Pipeline, node
+from kedro.pipeline import Pipeline, node, pipeline
+import kedro
 import numpy as np
 from .utils import validate, predict
 import h5py
 import torch
+import yaml
+from kedro.config import ConfigLoader
 from pydiver.models.lstm import STLSTM
 
 model_keys = dict(
@@ -13,6 +16,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def prediction_io(X, models_dataset, kwargs):   
+    #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",kwargs)
     for partition_id, partition_load_func in models_dataset.items(): 
         if partition_id == kwargs['name']:
             print("load model")
@@ -25,6 +29,7 @@ def prediction_io(X, models_dataset, kwargs):
     return {kwargs['name']: y_preds}
 
 
+
 #def validation_io(filepath_true, filepath_pred, kwargs):
 #    y_true = np.array(h5py.File(filepath_true, 'r')['Y'])
 #    y_pred = np.array(h5py.File(filepath_pred, 'r')[kwargs['model']])
@@ -34,8 +39,9 @@ def prediction_io(X, models_dataset, kwargs):
  #   return {kwargs['name']: loss}
 
 
-def create_pipeline(regime="A", **kwargs):
-    return Pipeline(
+def create_pipeline(regime="A", multi_model_eval=None):
+
+    model_eval_pipe = Pipeline(
         [
             node(
                 func=prediction_io,
@@ -43,12 +49,39 @@ def create_pipeline(regime="A", **kwargs):
                 outputs=f"regime_{regime}_pred",
                 name="prediction_node",
             ),  
-            
-            #node(
-            #    func=validation_io,
-            #    inputs=[f"regime_{regime}_test", f"regime_{regime}_pred", "params:validation"],
-            #    outputs=f"regime_{regime}_loss",
-            #    name="validation_node",
-            #),    
         ]
     )
+
+    if isinstance(multi_model_eval, dict):
+        multi_model_eval_pipes = Pipeline([])
+        
+        for name, depths in zip(multi_model_eval['model_names'], multi_model_eval['all_depths']):
+            d = {'name':name, 'depths':depths, 'device':'cuda'}#, **multi_model_eval}
+
+            def generate_param_node(param_to_return):
+                def generated_data_param():
+                    return param_to_return
+                return generated_data_param
+
+            pipeline_key = f'pipeline_{name}'
+
+            multi_model_eval_pipes += Pipeline([
+                node(
+                    generate_param_node(d), 
+                    inputs=None,
+                    outputs=pipeline_key
+                )
+            ])
+
+            multi_model_eval_pipes += pipeline(
+                model_eval_pipe,
+                inputs={f"regime_{regime}_X_test":f"regime_{regime}_X_test", 
+                        f"regime_{regime}_models":f"regime_{regime}_models"},
+                outputs={f"regime_{regime}_pred"},
+                parameters={"params:prediction":pipeline_key},
+                namespace=pipeline_key,
+            )
+
+        return multi_model_eval_pipes
+
+    else: return model_eval_pipe
