bd6bfe686719448bc7b9d179e0dc596d2675e542
RolandGit95 <rolandstenger95@gmail.com>
2021-09-16 14:45:01 +0200

    updated

--- src/pydiver/pipeline_registry.py
+++ src/pydiver/pipeline_registry.py
@@ -6,23 +6,23 @@ import kedro
 from kedro.pipeline import Pipeline
 from kedro.config import ConfigLoader
 
-from pydiver.pipelines import data_simulation as ds
-from pydiver.pipelines import data_processing as dp
+#from pydiver.pipelines import data_simulation as ds
+#from pydiver.pipelines import data_processing as dp
 from pydiver.pipelines import model_validation as mv
-from pydiver.pipelines import training as tr
+#from pydiver.pipelines import training as tr
 
         
 def register_pipelines() -> Dict[str, Pipeline]:
 
     registered_pipelines = {}
 
-    data_simulation_pipeline = ds.create_pipeline()
+    #data_simulation_pipeline = ds.create_pipeline()
     model_validation_pipeline = mv.create_pipeline()
-    training_pipeline = tr.create_pipeline()
+    #training_pipeline = tr.create_pipeline()
 
-    registered_pipelines['ds'] = data_simulation_pipeline
+    #registered_pipelines['ds'] = data_simulation_pipeline
     registered_pipelines['mv'] = model_validation_pipeline
-    registered_pipelines['tr'] = training_pipeline
+    #registered_pipelines['tr'] = training_pipeline
 
     registered_pipelines['__default__'] = model_validation_pipeline
 
