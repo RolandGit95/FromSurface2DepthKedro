55a6ac860b3a9b05c65b90102dae2e27920010aa
RolandGit95 <rolandstenger95@gmail.com>
2021-10-11 11:41:18 +0200

    updated

--- src/pydiver/pipeline_registry.py
+++ src/pydiver/pipeline_registry.py
@@ -17,13 +17,13 @@ def register_pipelines() -> Dict[str, Pipeline]:
 
     #data_simulation_pipeline = ds.create_pipeline()
     model_validation_pipeline = mv.create_pipeline()
-    training_pipeline = tr.create_pipeline()
+    #training_pipeline = tr.create_pipeline()
     training_pipeline_without_pl = tr.create_pipeline_without_pl()
 
     #registered_pipelines['ds'] = data_simulation_pipeline
     registered_pipelines['mv'] = model_validation_pipeline
-    registered_pipelines['tr'] = training_pipeline
-    registered_pipelines['tr+mv'] = training_pipeline + model_validation_pipeline
+    #registered_pipelines['tr'] = training_pipeline
+    #registered_pipelines['tr+mv'] = training_pipeline + model_validation_pipeline
 
     registered_pipelines['tr_without_pl'] = training_pipeline_without_pl
     registered_pipelines['tr_without_pl+mv'] = training_pipeline_without_pl + model_validation_pipeline
