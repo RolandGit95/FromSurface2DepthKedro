97db482e92628f19915f4a82e3830a1608fd6bcf
RolandGit95 <rolandstenger95@gmail.com>
2021-10-26 13:35:08 +0200

    updated

--- training.py
+++ training.py
@@ -41,6 +41,7 @@ def main(argv):
     name = f'STLSTM_t_0_2_4_6_8_10_12_14_16_18_20_22_24_26_28_30_31_d_{SGE_TASK_ID}'
     depths = str([SGE_TASK_ID])
     time_steps = str([0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,31])
+    time_steps = time_steps.replace(',','_')
 
     ds = 'data_science'
     os.system(f"kedro run --env mpi_regimeB --pipeline tr_without_pl+mv --params {ds}.name:{name},{ds}.depths:{depths},{ds}.time_steps:{time_steps}")
