3b130d8c25b16dc498e1b85642f730ccfbf39eea
RolandGit95 <rolandstenger95@gmail.com>
2021-11-11 14:21:58 +0100

    iüdazed

---
+++ exp3_var_lr.py
@@ -0,0 +1,45 @@
+#! python
+# %%
+# name
+#$ -N kedro
+
+# execute from current directory
+#$ -cwd
+
+# Preserve environment variables
+#$ -V
+
+# Provide path to python executable
+#$ -S /home/stenger/smaxxhome/anaconda3/envs/pydiver/bin/python
+
+# Merge error and out
+#$ -j yes
+
+# serial queue
+#$ -q taranis-gpu1.q
+# -q teutates.q
+# -q grannus.q
+
+# Path for output
+#$ -o /data.bmp/heart/DataAnalysis/2020_3DExMedSurfaceToDepth/FromSurface2DepthKedro/logs
+
+# job array of length 1
+# -t 1:2
+# %%
+
+import os
+import numpy as np
+
+def main():
+
+    learning_rates = np.arange(0.0001,0.001,0.0001) # len: 9 --> -t 0:8
+    print(os.environ['SGE_TASK_ID'])
+    SGE_TASK_ID = int(os.environ['SGE_TASK_ID'])
+
+    name = f'STLSTM_t32_d_{SGE_TASK_ID}'
+    depths = f"[16]"
+    lr = learning_rates[SGE_TASK_ID]
+    os.system(f"kedro run --env exp3_mpi --pipeline tr_without_pl+mv --params data_science.name:{name},data_science.depths:{depths},data_science.lr:{lr}")
+
+if __name__=='__main__':
+    main()
